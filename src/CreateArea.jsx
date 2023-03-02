00df29276fe0510351fd721832d70c318b6243b6
shivam23kumar <shivamkumar.dev23@gmail.com>
2023-03-02 22:59:59 +0530

    createArea

--- src/CreateArea.jsx
+++ src/CreateArea.jsx
@@ -1,4 +1,7 @@
 import React,{useState} from "react";
+// import AddIcon from "@material-ui/icons/Add";
+// import Fab from "@material-ui/core/Fab";
+// import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
     const [note, setNote] = useState({
@@ -28,7 +31,7 @@ function CreateArea(props) {
    
   return (
     <div>
-      <form>
+      <form className="create-note">
         <input 
         name="title" 
         placeholder="Title" 
